498a41fd738b0a5a341300409cd80d310444e0a2
陈鑫荣 <chenxinrong@chenxinrongdeMacBook-Pro.local>
2017-05-11 07:24:10 +0800

    Initial Commit

---
+++ UWeibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UWeibo
+//
+//  Created by 陈鑫荣 on 2017/5/11.
+//  Copyright © 2017年 justprint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UWeibo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UWeibo
+//
+//  Created by 陈鑫荣 on 2017/5/11.
+//  Copyright © 2017年 justprint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
