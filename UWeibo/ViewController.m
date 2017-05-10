db5d1bf4eeae84f684abf8e0e025c19fb79bdcca
陈鑫荣 <chenxinrong@chenxinrongdeMacBook-Pro.local>
2017-05-11 07:44:46 +0800

    helloworld

--- UWeibo/ViewController.m
+++ UWeibo/ViewController.m
@@ -16,7 +16,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSLog(@"---");
+    NSLog(@"hello world");
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
