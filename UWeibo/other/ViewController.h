ac2f95acd6a3d841fd1b1fd59eb9621529480484
陈鑫荣 <chenxinrong@chenxinrongdeMacBook-Pro.local>
2017-05-12 12:52:26 +0800

    微博头像更改圆形

---
+++ UWeibo/other/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  yinyue
+//
+//  Created by 陈鑫荣 on 2017/4/11.
+//  Copyright © 2017年 justprint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UWeibo/other/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  yinyue
+//
+//  Created by 陈鑫荣 on 2017/4/11.
+//  Copyright © 2017年 justprint. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()<UIScrollViewDelegate>
+@property (strong, nonatomic)  UIScrollView *scrollView;
+@property (strong, nonatomic)  UIPageControl *pageControl;
+@property(nonatomic,strong)NSTimer* timer;
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+@end
